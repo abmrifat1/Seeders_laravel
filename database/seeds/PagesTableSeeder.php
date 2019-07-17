ce48dad032554060aa2238ad5bc7632cced9a47a
abmrifat1 <rifatabm1@gmail.com>
2019-07-17 16:11:26 +0600

    first commit

---
+++ database/seeds/PagesTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PagesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+       DB::table('pages')->insert([[
+            'title'=>'About Page',
+            'slug'=>'about',
+            'content'=>'This is About pages',
+       ],
+           [
+               'title'=>'Blog Page',
+               'slug'=>'blog',
+               'content'=>'This is Blog pages',
+           ],
+           [
+               'title'=>'Contact Page',
+               'slug'=>'contact',
+               'content'=>'This is Contact pages',
+           ]
+       ]);
+    }
+}
